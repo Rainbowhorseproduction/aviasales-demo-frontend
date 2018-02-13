c210cd8da1852e740bdc39b0f90c9fda72126fcd
rainbowhorseproduction <rainbowhorseproduction@gmail.com>
2018-02-13 22:21:01 +0300

    start feat/main-page branch

---
+++ src/Intro.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function() {
+  return (
+    <div className="App-intro">
+      <p>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
